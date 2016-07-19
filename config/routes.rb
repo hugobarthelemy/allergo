ce89b19146c81be241ac4f56f24215a3be26fc8e
hugo barthelemy <barthelemyhugo@gmail.com>
2016-07-20 00:16:42 +0200

    add user show, user edit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
+  resources :users, only: [:show, :edit, :update]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
