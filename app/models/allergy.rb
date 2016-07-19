a6a391cc1aac9c9cbb44140a69276462da757d41
Antoine Reveau <antoinerev@gmail.com>
2016-07-20 01:03:43 +0200

    fixed allergy model

--- app/models/allergy.rb
+++ app/models/allergy.rb
@@ -1,4 +1,6 @@
 class Allergy < ActiveRecord::Base
   has_many :levels
-  has_many :allergies_ingredients
+  has_many :allergies_ingredients, dependent: :destroy
+
+  has_many :ingredients, through: :allergies_ingredients
 end
