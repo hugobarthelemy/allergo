3191e8f37d812623d36ae889bf75e0c6b5fb2247
Antoine Reveau <antoinerev@gmail.com>
2016-07-20 19:24:23 +0200

    mailer alert created

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,12 +5,12 @@ class UserMailer < ApplicationMailer
   #
   #   en.user_mailer.alert.subject
   #
-  def alert
+  def alert(user, product_changed)
     @greeting = "Hi"
-    @products_changed ################ TODO
+    @product_changed = product_changed
 
     @user = user  # Instance variable => available in view
-    @user_name = "@user.first_name @user.last_name"
+    @user_name = "#{@user.first_name} #{@user.last_name}"
 
     mail(to: @user.email, subject: 'Product Change Alert')
     # This will render a view in `app/views/user_mailer`!
