836e37483b32cfa7908b6e1db99901ecaaa05a34
Laura Pedroni <laurage_@hotmail.com>
2016-07-20 17:05:29 +0200

    controller product - not ready yet

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,17 @@
 class ProductsController < ApplicationController
+  before_action :product_params, only: [:create, :show]
+  before_action :set_product, only: [:create, :show]
   def index
     @products = policy_scope(Product)
   end
 
   def new
+    @product = Product.new
+    authorize @product
   end
 
   def create
+    authorize @product
   end
 
   def show
@@ -19,5 +24,13 @@ class ProductsController < ApplicationController
   end
 
   def destroy
+  end
+
+  def product_params
+    params.require(:product).permit(:id, :barcode, :name, :updated_on, :manufacturer, :category)
+  end
+
+  def set_product
+    @product = Product.find(product_params)
   end
 end
--- app/policies/product_policy.rb
+++ app/policies/product_policy.rb
@@ -8,4 +8,16 @@ class ProductPolicy < ApplicationPolicy
   def index?
     true
   end
+
+  def show?
+    true
+  end
+
+  def update?
+    true # tous les users signés peuvent editer
+  end
+
+  def create?
+    true # tous les utilisateurs peuvent créer un user
+  end
 end
