7cdeebbb0979b37d768605a2869d00a39160c8e1
paulgaumer <mitsudomoe8@gmail.com>
2016-07-21 16:47:23 +0200

    Barcode scanner working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,13 @@
 class PagesController < ApplicationController
   skip_after_action :verify_authorized
   def home
+    if params[:scanresult]
+      @barcode = params[:scanresult]
+      if @product = Product.find_by(barcode: @barcode)
+        redirect_to product_path(@product)
+      else
+        @message = "Cannot read barcode. Try again."
+      end
+    end
   end
 end
