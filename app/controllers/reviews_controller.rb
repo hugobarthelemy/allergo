34fdce164116d8579852654c08cd7c11a562d559
paulgaumer <mitsudomoe8@gmail.com>
2016-07-22 16:11:01 +0200

    Can create review but no authenticate @user

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,26 @@
+class ReviewsController < ApplicationController
+  before_action :find_product, only: [ :new, :create ]
+  skip_after_action :verify_authorized
+
+  def new
+    @review = Review.new
+    # authorize @review
+  end
+
+  def create
+    @review = @product.reviews.build(review_params)
+    @review.save
+    # authorize @review
+    redirect_to product_path(@product)
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:score, :content)
+  end
+
+  def find_product
+    @product = Product.find(params[:product_id])
+  end
+end
---
+++ app/policies/review_policy.rb
@@ -0,0 +1,15 @@
+class ReviewPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+
+    def new?
+      true
+    end
+
+    def create?
+      true
+    end
+  end
+end
