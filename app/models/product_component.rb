af613eff6048e54b6ffa7aeff53efa1d4efa8096
Antoine Reveau <antoinerev@gmail.com>
2016-07-25 13:01:19 +0200

    added modified product_component model

--- app/models/product_component.rb
+++ app/models/product_component.rb
@@ -2,40 +2,40 @@ class ProductComponent < ActiveRecord::Base
   belongs_to :ingredient
   belongs_to :product
 
-  def self.create_trace_from_api(trace_name, new_product)
-    if trace_name.include?(":")
-      trace = trace_name.split(":")
-      case trace[0]
+  def self.create_allergen_or_trace_from_api(ingredient_api_name, new_product, amount)
+    if ingredient_api_name.include?(":")
+      ingredient_api = ingredient_api_name.split(":")
+      case ingredient_api[0]
       when "fr"
-        ingredient = Ingredient.find_or_create_by(fr_name: trace[1])
+        ingredient = Ingredient.find_or_create_by(fr_name: ingredient_api[1])
       when "en"
-        ingredient = Ingredient.find_or_create_by(en_name: trace[1])
+        ingredient = Ingredient.find_or_create_by(en_name: ingredient_api[1])
       when "ja"
-        ingredient = Ingredient.find_or_create_by(ja_name: trace[1])
+        ingredient = Ingredient.find_or_create_by(ja_name: ingredient_api[1])
       else
         ### TODO ### create "world_name" for unknown language
-        ingredient = Ingredient.find_or_create_by(en_name: trace[1])
+        ingredient = Ingredient.find_or_create_by(en_name: ingredient_api[1])
       end
     else
-      if ingredient = Ingredient.find_by(fr_name: trace_name)
+      if ingredient = Ingredient.find_by(fr_name: ingredient_api_name)
         ingredient
       else
-        if ingredient = Ingredient.find_by(en_name: trace_name)
+        if ingredient = Ingredient.find_by(en_name: ingredient_api_name)
           ingredient
         else
-          if ingredient = Ingredient.find_by(ja_name: trace_name)
+          if ingredient = Ingredient.find_by(ja_name: ingredient_api_name)
             ingredient
           else
             ### TODO ### create "world_name" for unknown language
-            ingredient = Ingredient.create(en_name: trace_name)
+            ingredient = Ingredient.create(en_name: ingredient_api_name)
           end
         end
       end
     end
 
-    trace_products = ProductComponent.where(product_id: new_product.id,
+    trace_or_allergen_products = ProductComponent.where(product_id: new_product.id,
                                               ingredient_id: ingredient.id,
-                                              amount: 1
+                                              amount: amount
                                             ).first_or_create
   end
 end
