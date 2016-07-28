0b2ee6e94d339ec45f27c8ee696653454738776e
Antoine Reveau <antoinerev@gmail.com>
2016-07-28 17:08:04 +0200

    revert to one mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -13,14 +13,10 @@ class UserMailer < ApplicationMailer
     @action = action
 
     @destination_email = "barthelemyhugo@gmail.com"
-    @destination_email_two = "antoinerev@gmail.com"
     @user_name = "Hugo Barthelemy"
     # @user_name = "#{@user.first_name} #{@user.last_name}"
 
     mail(to: @destination_email, subject: 'Product Change Alert')
-
-    @user_name = "Antoine Reveau"
-    mail(to: @destination_email_two, subject: 'Product Change Alert')
     # This will render a view in `app/views/user_mailer`!
   end
 
